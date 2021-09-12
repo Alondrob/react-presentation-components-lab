358174d6815dfc10c9efebfa6ed7ae4c1c27fe43
Alondrob <alondrob@gmail.com>
2021-09-12 15:10:21 -0400

    commit tree 127701fcb65248d58358697b383d8ca475c6a871

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,38 @@
 // Code SimpleComponent Here
+
+import React, { Component } from 'react'
+
+class SimpleComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            mood: 'happy'
+        }
+    }
+
+     handleClick = () => {
+         const newMood = this.state.mood == 'happy' ? 'sad' :'happy'
+         this.setState({mood:newMood})
+        // this.setState(prevState => {
+        //     console.log(prevState)
+        //     prevState.mood === 'happy' ? 'sad' :'happy'
+       
+       
+    }
+    
+    
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+             
+                    {this.state.mood}
+                    
+              </div>
+        )
+    }
+    
+}
+
+export default SimpleComponent
+
